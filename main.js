0ddd504ca073d31ce6e2e27ecb985c22d5ef197f
Amira Khaled <155392660+amirakhaled2027@users.noreply.github.com>
2024-08-09 23:39:01 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+//Given two arguments, return an array which contains these two arguments.
+
+function makePair(num1, num2) {
+	return [num1, num2]
+}
+console.log(makePair(1, 2));
+console.log(makePair(51, 21));
+console.log(makePair(512124, 215));
